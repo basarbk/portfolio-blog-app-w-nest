24b2de0c7d4667b66aac473bcefc063f71bb47e8
Basar Buyukkahraman <basarbk@yahoo.com>
2024-05-26 17:19:18 +0300

    0201-controller

---
+++ src/user/user.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+
+@Controller('users')
+export class UserController {
+  @Post()
+  @HttpCode(HttpStatus.OK)
+  createUser() {
+    return 'Success';
+  }
+}
