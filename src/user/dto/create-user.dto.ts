ef125a504ec526cd9fc9334bed233c0a525d4e21
Basar Buyukkahraman <basarbk@yahoo.com>
2024-05-27 23:29:08 +0300

    0404-unique-email-validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,8 @@
-import { IsEmail } from 'class-validator';
+import { IsEmail, Validate } from 'class-validator';
+import { UniqueEmail } from '../validator/unique-email.validator';
 
 export class CreateUser {
   @IsEmail({}, { message: 'Invalid email format' })
+  @Validate(UniqueEmail)
   email: string;
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,10 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { EmailModule } from '../email/email.module';
+import { UniqueEmail } from './validator/unique-email.validator';
 
 @Module({
-  providers: [UserService],
+  providers: [UserService, UniqueEmail],
   controllers: [UserController],
   imports: [TypeOrmModule.forFeature([User]), EmailModule],
 })
