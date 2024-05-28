f18cb42695f0e5ade9c8c6898eeebf2fab192b8c
Basar Buyukkahraman <basarbk@yahoo.com>
2024-05-28 22:27:43 +0300

    0602-save-article

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ArticleService } from './article.service';
+import { ArticleRequest } from './dto/article-request.dto';
 
-@Controller('article')
+@Controller('articles')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
+
+  @Post()
+  async createArticle(@Body() body: ArticleRequest): Promise<{ id: number }> {
+    return this.articleService.save(body);
+  }
 }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { ArticleRequest } from './dto/article-request.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Article } from './article.entity';
+import { Repository } from 'typeorm';
+import { generateUniqueValue } from '../shared';
 
 @Injectable()
-export class ArticleService {}
+export class ArticleService {
+  constructor(
+    @InjectRepository(Article) private articleRepository: Repository<Article>,
+  ) {}
+
+  async save(value: ArticleRequest): Promise<{ id: number }> {
+    const article = new Article();
+    article.title = value.title;
+    article.content = value.content;
+    article.image = value.image;
+    article.slug =
+      encodeURIComponent(value.title.toLocaleLowerCase().replaceAll(' ', '-')) +
+      '-' +
+      generateUniqueValue(true);
+
+    await this.articleRepository.save(article);
+    return { id: article.id };
+  }
+}
