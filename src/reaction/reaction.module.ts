4928415844b424da4f2861685c4e1780f4d37bdd
Basar Buyukkahraman <basarbk@yahoo.com>
2024-05-30 23:47:56 +0300

    1002-save-reaction

--- src/reaction/reaction.module.ts
+++ src/reaction/reaction.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ReactionService } from './reaction.service';
 import { ReactionController } from './reaction.controller';
+import { AuthUserMiddleware } from '../auth/auth-user.middleware';
+import { AuthModule } from '../auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Reaction } from './reaction.entity';
 
 @Module({
+  imports: [AuthModule, TypeOrmModule.forFeature([Reaction])],
   controllers: [ReactionController],
   providers: [ReactionService],
 })
-export class ReactionModule {}
+export class ReactionModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthUserMiddleware).forRoutes('reactions');
+  }
+}
--- src/reaction/reaction.service.ts
+++ src/reaction/reaction.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Reaction } from './reaction.entity';
+import { Repository } from 'typeorm';
+import { ReactionDTO } from './dto/reaction.dto';
+import { User } from '../user/user.entity';
 
 @Injectable()
-export class ReactionService {}
+export class ReactionService {
+  constructor(
+    @InjectRepository(Reaction)
+    private reactionRepository: Repository<Reaction>,
+  ) {}
+
+  async toggleReaction(
+    reactionDto: ReactionDTO,
+    user: User,
+  ): Promise<{ result: boolean }> {
+    const reactionInDB = await this.reactionRepository.findOne({
+      where: {
+        category: reactionDto.category,
+        article: {
+          id: reactionDto.entityId,
+        },
+        user: {
+          id: user.id,
+        },
+      },
+    });
+    if (reactionInDB) {
+      await this.reactionRepository.delete(reactionInDB);
+      return { result: false };
+    } else {
+      try {
+        await this.reactionRepository.save({
+          category: reactionDto.category,
+          article: { id: reactionDto.entityId },
+          user,
+        });
+        return { result: true };
+      } catch {
+        throw new NotFoundException('Entity not found');
+      }
+    }
+  }
+}
